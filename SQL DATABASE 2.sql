6ec125a4722794756be2b349b8a5b5a38a7aab12
Rahila074 <rahilamuhammed074@gmail.com>
2024-10-15 13:23:08 +0530

    Add files via upload

---
+++ SQL DATABASE 2.sql
@@ -0,0 +1,14 @@
+SHOW DATABASES;
+USE students;
+show tables;
+select * from students1;
+insert into students1(ENTROLL_NO,STUDENTS_NAME,DOB,GENDER,AGE)
+VALUES(1,"RAHILA","2024-03-03","F",21);
+
+insert into students1(ENTROLL_NO,STUDENTS_NAME,DOB,GENDER,AGE)
+VALUES(2,"HENA","2024-03-03","F",21),
+	  (3,"FIDHA","2024-03-03","F",21),
+      (4,"NAJIYA","2024-03-03","F",21),
+      (5,"RISHU","2024-03-03","F",21);
+      
+      SELECT * FROM STUDENTS1;
